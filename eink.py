8d5433621b08683c60a115c4e9d4443b22275652
Arthur Guy <arthur@assys.net>
2020-03-21 17:18:33 +0000

    no message

--- eink.py
+++ eink.py
@@ -53,7 +53,7 @@ def draw_text(position, text, font=None, colour=inky_display.BLACK, rotation=0):
     img.paste(colour, position, mask)
 
 
-inky_display.set_border(inky_display.BLACK)
+# inky_display.set_border(inky_display.BLACK)
 
 
 # for y in range(0, 10):
@@ -86,16 +86,16 @@ inky_display.set_border(inky_display.BLACK)
 # draw_text((80, 20), "World", rotation=90)
 
 y = 0
-draw_text((0, y), "Sun 22 Mar 00:00")
-draw_text((0, y + 14), "Mother's Day")
+draw_text((5, y), "Sun 22 Mar - All day")
+draw_text((5, y + 14), "Mother's Day", colour=inky_display.RED)
 
 y += 35
-draw_text((0, y), "Mon 23 Mar 13:00")
-draw_text((0, y + 14), "Interview 2")
+draw_text((5, y), "Mon 23 Mar 13:00")
+draw_text((5, y + 14), "Interview 2")
 
 y += 35
-draw_text((0, y), "Wed 25 Mar 14:00")
-draw_text((0, y + 14), "1 to 1 - John & Smith")
+draw_text((5, y), "Wed 25 Mar 14:00")
+draw_text((5, y + 14), "1 to 1 - John & Smith")
 
 inky_display.set_image(img)
 inky_display.show()
