a53ff853a930439087de7a4f20f00904f87e9f86
Arthur Guy <arthur@assys.net>
2020-03-22 20:09:59 +0000

    no message

--- eink.py
+++ eink.py
@@ -105,22 +105,32 @@ y = 0
 # draw_text((0, y), "16:00 1 to 1 - John & Smith")
 
 
+today = datetime.datetime.now()
+tomorrow = today + datetime.timedelta(days=1)
 events = get_all_calendar_items()
+working_day_ended = False
 for event in events:
     start_time = datetime.datetime.fromisoformat(event.get('start_time'))
-    if event.get('duration') == 0:
-        text = 'All day '
-    else:
-        text = start_time.strftime("%H:%M ")
-    draw_text((0, y), text + event.get('summary'))
-    y += 18
-    # print(start_time.strftime("%A %d %B %Y %H:%M"), event.get('duration'), event.get('summary'))
+    if working_day_ended and start_time.date() == tomorrow.date():
+        if event.get('duration') == 0:
+            text = 'All day '
+        else:
+            text = start_time.strftime("%H:%M ")
+        draw_text((0, y), text + event.get('summary'))
+        y += 18
+    elif start_time.date() == today.date():
+        if event.get('duration') == 0:
+            text = 'All day '
+        else:
+            text = start_time.strftime("%H:%M ")
+        draw_text((0, y), text + event.get('summary'))
+        y += 18
 
 
 # Dispay the date in the bottom right on a black background
 date_text = "22/03/2020"
-for y in range(85, inky_display.height):
-    for x in range(135 - (y - 85), inky_display.width):
+for y in range(86, inky_display.height):
+    for x in range(138 - (y - 86), inky_display.width):
         img.putpixel((x, y), inky_display.BLACK)
 draw_text((138, 88), date_text, colour=inky_display.WHITE)
 
