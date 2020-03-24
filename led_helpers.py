eb3ab468f755cb67f6d4c00ebaeb1409a55275e1
Arthur Guy <arthur@assys.net>
2020-03-24 07:59:10 +0000

    no message

--- led_helpers.py
+++ led_helpers.py
@@ -74,6 +74,8 @@ def determine_highlight_pixels(hour, minute, minute_duration):
 def highlight_event_time(hour, minute, minute_duration, hew, brightness, saturation=1):
     pixels = determine_highlight_pixels(hour, minute, minute_duration)
     num_leds = len(pixels)
+    if num_leds is 0:
+        return
     pixel = pixels[0]
 
     if pixel is not None:
