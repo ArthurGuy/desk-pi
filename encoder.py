0880410920b5340de6b607fc61ea647da85bca7d
Arthur Guy <arthur@assys.net>
2020-03-26 16:48:15 +0000

    no message

--- encoder.py
+++ encoder.py
@@ -5,7 +5,8 @@ from time import sleep
 Enc_A = 23  # Encoder input A
 Enc_B = 24  # Encoder input B
 
-counter = 10
+counter = 0
+counter_max = 5
 
 
 def init():
@@ -30,31 +31,7 @@ def init():
 
 
 def rotation_decode(Enc_A):
-    '''
-    This function decodes the direction of a rotary encoder and in- or
-    decrements a counter.
-
-    The code works from the "early detection" principle that when turning the
-    encoder clockwise, the A-switch gets activated before the B-switch.
-    When the encoder is rotated anti-clockwise, the B-switch gets activated
-    before the A-switch. The timing is depending on the mechanical design of
-    the switch, and the rotational speed of the knob.
-
-    This function gets activated when the A-switch goes high. The code then
-    looks at the level of the B-switch. If the B switch is (still) low, then
-    the direction must be clockwise. If the B input is (still) high, the
-    direction must be anti-clockwise.
-
-    All other conditions (both high, both low or A=0 and B=1) are filtered out.
-
-    To complete the click-cycle, after the direction has been determined, the
-    code waits for the full cycle (from indent to indent) to finish.
-
-    '''
-
     global counter
-
-    sleep(0.002) # extra 2 mSec de-bounce time
 
     # read both of the switches
     Switch_A = GPIO.input(Enc_A)
@@ -62,10 +39,12 @@ def rotation_decode(Enc_A):
 
     if (Switch_A == 0) and (Switch_B == 1):  # A then B ->
         counter += 1
+        if counter > counter_max:
+            counter = 0
         print("direction -> ", counter)
-        # at this point, B may still need to go high, wait for it
         while Switch_B == 1:
             Switch_B = GPIO.input(Enc_B)
+
         # now wait for B to drop to end the click cycle
         while Switch_B == 0:
             Switch_B = GPIO.input(Enc_B)
@@ -73,14 +52,15 @@ def rotation_decode(Enc_A):
 
     elif (Switch_A == 0) and (Switch_B == 0):  # B then A <-
         counter -= 1
+        if counter < 0:
+            counter = counter_max
         print("direction <- ", counter)
-         # A is already high, wait for A to drop to end the click cycle
         while Switch_A == 0:
             Switch_A = GPIO.input(Enc_A)
         return
 
-    else: # discard all other combinations
-        return
+    else:  # discard all other combinations
+        return None
 
 
 def main():
