10e79434e67f66402bd65cfc4f0af39ed98d9ee7
Arthur Guy <arthur@assys.net>
2020-04-11 16:24:19 +0100

    no message

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ inky_display = InkyPHAT('red')
 # label_font = ImageFont.truetype(HankenGroteskMedium, int(13))
 bold_font = ImageFont.truetype('SubVario-Condensed-Medium.otf', int(17))
 
-label_font = ImageFont.truetype(AmaticSCBold, 13)
+label_font = ImageFont.truetype(FredokaOne, 13)
 
 
 # Canvas
