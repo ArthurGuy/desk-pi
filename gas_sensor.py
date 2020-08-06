55c515c0705025c40c691f65c2c287283f735c2f
Arthur Guy <arthur@assys.net>
2020-08-06 15:42:56 +0100

    Updated example file

--- gas_sensor.py
+++ gas_sensor.py
@@ -1,24 +1,17 @@
-from sgp30 import SGP30
+from smbus2 import SMBusWrapper
+from sgp30 import Sgp30
 import time
-import sys
-
-sgp30 = SGP30()
-
-# result = sgp30.command('set_baseline', (0xFECA, 0xBEBA))
-# result = sgp30.command('get_baseline')
-# print(["{:02x}".format(n) for n in result])
-
-print("Sensor warming up, please wait...")
-def crude_progress_bar():
-    sys.stdout.write('.')
-    sys.stdout.flush()
-
-sgp30.start_measurement(crude_progress_bar)
-sys.stdout.write('\n')
-
-while True:
-    result = sgp30.get_air_quality()
-    print(result)
-    time.sleep(1.0)
-
-# sudo pip install pimoroni-sgp30
+with SMBusWrapper(1) as bus:
+    sgp = Sgp30(bus, baseline_filename="/tmp/mySGP30_baseline")  # things thing with the baselinefile is dumb and will be changed in the future
+    print("resetting all i2c devices")
+    sgp.i2c_geral_call()  # WARNING: Will reset any device on teh i2cbus that listens for general call
+    print(sgp.read_features())
+    print(sgp.read_serial())
+    sgp.init_sgp()
+    print(sgp.read_measurements())
+    print("the SGP30 takes at least 15 seconds to warm up, 12 hours before the readigs become really stable")
+    for i in range(20):
+        time.sleep(1)
+        print(".", end="")
+    print()
+    print(sgp.read_measurements())
